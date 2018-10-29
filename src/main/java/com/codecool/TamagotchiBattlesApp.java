b40a2058fea9226b9856feea850f742801d453fe
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-10-29 20:11:20 +0100

    Initial commit

---
+++ src/main/java/com/codecool/TamagotchiBattlesApp.java
@@ -0,0 +1,13 @@
+package com.codecool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagotchiBattlesApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(TamagotchiBattlesApp.class, args);
+
+    }
+}
