9e105a9051726f1a797cc9c6900cac1cf4abf494
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-10-31 09:52:13 +0100

    Fixed main controller

--- src/main/java/com/codecool/Controller/MainController.java
+++ src/main/java/com/codecool/Controller/MainController.java
@@ -1,13 +1,13 @@
 package com.codecool.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
-    public String getProducts(){
+    public String home(){
         return "index";
     }
 
